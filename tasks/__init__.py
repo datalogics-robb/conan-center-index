3050c3e8fdafd7722b729478d219a4547d6a94d3
Kevin A. Mitchell <kam@datalogics.com>
2021-11-18 17:17:34 -0600

    upload-recipes: Run in parallel via ThreadPool

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,15 +4,19 @@ import yaml
 from dl_conan_build_tools.tasks import conan
 from invoke import Collection
 from invoke.tasks import Task, task
+from multiprocessing.pool import ThreadPool
 
 
 @task(help={'remote': 'remote to upload to, default conan-center-dl-staging',
             'package': 'name of package to upload, can be specified more than once',
             'all': 'upload all packages in recipes folder',
-            'since-commit': 'upload all packages in recipes folder changed since COMMIT'
+            'since-commit': 'upload all packages in recipes folder changed since COMMIT',
+            'parallel': 'run uploads in parallel (default)',
+            'upload': 'upload the recipe (default) (otherwise, just does the exports)'
             },
       iterable=['package'])
-def upload_recipes(ctx, remote='conan-center-dl-staging', package=None, all=False, since_commit=None):
+def upload_recipes(ctx, remote='conan-center-dl-staging', package=None, all=False, since_commit=None, parallel=True,
+                   upload=True):
     """Export and upload the named recipes to the given remote.
 
     Exports and uploads all the versions of the selected recipes to the remote."""
@@ -29,11 +33,19 @@ def upload_recipes(ctx, remote='conan-center-dl-staging', package=None, all=Fals
     print('*** Uploading:')
     for pkg in sorted_packages:
         print(f'    {pkg}')
-    for one_package in sorted_packages:
-        upload_one_package_name(ctx, one_package, remote)
+
+    def do_upload(one_package):
+        upload_one_package_name(ctx, one_package, remote, upload=upload)
+
+    if parallel:
+        with ThreadPool() as pool:
+            pool.map(do_upload, sorted_packages)
+    else:
+        for one_package in sorted_packages:
+            do_upload(one_package)
 
 
-def upload_one_package_name(ctx, package_name, remote):
+def upload_one_package_name(ctx, package_name, remote, upload=True):
     """Upload one recipe to the given remote"""
     ctx.run(f'conan remove {package_name} --force')
     recipe_folder = os.path.join('recipes', package_name)
@@ -51,7 +63,8 @@ def upload_one_package_name(ctx, package_name, remote):
                     version = entry.name
                     folder = os.path.join(recipe_folder, version)
                     ctx.run(f'conan export {folder} {package_name}/{version}@')
-    ctx.run(f'conan upload -r {remote} {package_name} --confirm')
+    if upload:
+        ctx.run(f'conan upload -r {remote} {package_name} --confirm')
 
 
 tasks = []
