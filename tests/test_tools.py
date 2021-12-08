b8ec990fd13ce7a2a0eb0be9c5c6bdd721dc6814
Kevin A. Mitchell <kam@datalogics.com>
2021-12-08 13:55:44 -0600

    Support adding options to prebuilt_tools.
    - Can build multiple tools with different options.
    
    - The list of prebuilt_tools can contain a string, which is the package name, or
      a dictionary with keys package and options.
    
    - The options are a list of package:option=value strings.

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,4 +1,5 @@
 import subprocess
+from typing import NamedTuple, List
 
 import dl_conan_build_tools.config
 import pytest
@@ -9,8 +10,26 @@ from util import recipes
 _config = dl_conan_build_tools.config.get_config()
 
 
+class Package(NamedTuple):
+    package: str
+    options: List[str]
+
+    def __str__(self):
+        result = self.package
+        if self.options:
+            result = f'{self.package}_{"_".join(self.options)}'
+        return result
+
+    @classmethod
+    def from_str_or_dict(cls, str_or_dict):
+        if isinstance(str_or_dict, str):
+            return cls(str_or_dict, [])
+        return cls(**str_or_dict)
+
+
 @pytest.fixture(scope='package',
-                params=_config.get('prebuilt_tools', []))
+                params=[Package.from_str_or_dict(entry) for entry in _config.get('prebuilt_tools', [])],
+                ids=lambda param: str(param))
 def prebuilt_tool(request):
     return request.param
 
@@ -29,17 +48,21 @@ def prebuilt_tool_config(request):
 
 @pytest.fixture(scope='package')
 def tool_recipe_folder(prebuilt_tool):
-    package, version = prebuilt_tool.split('/')
+    package, version = prebuilt_tool.package.split('/')
     return recipes.versions_to_folders(package)[version]
 
 
 class TestBuildTools(object):
     def test_build_tool(self, prebuilt_tool, prebuilt_tool_config, tool_recipe_folder, upload_to):
-        args = ['conan', 'create', tool_recipe_folder, f'{prebuilt_tool}@',
-                '--update'] + prebuilt_tool_config.install_options()
-        print(f'Creating package {prebuilt_tool}: {" ".join(args)}')
+        tool_options = []
+        for opt in prebuilt_tool.options:
+            tool_options.append('--options:host')
+            tool_options.append(opt)
+        args = ['conan', 'create', tool_recipe_folder, f'{prebuilt_tool.package}@',
+                '--update'] + prebuilt_tool_config.install_options() + tool_options
+        print(f'Creating package {prebuilt_tool.package}: {" ".join(args)}')
         subprocess.run(args, check=True)
         if upload_to:
-            args = ['conan', 'upload', '-r', upload_to, f'{prebuilt_tool}@', '--all', '--check']
-            print(f'Uploading {prebuilt_tool}: {" ".join(args)}')
-            subprocess.run(args, check=True)
+            args = ['conan', 'upload', '-r', upload_to, f'{prebuilt_tool.package}@', '--all', '--check']
+        print(f'Uploading {prebuilt_tool.package}: {" ".join(args)}')
+        subprocess.run(args, check=True)
