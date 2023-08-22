d8b23aeb2eeeb7a1f2191ff8ac247214ce59a7a4
Sahara Yousuf <saharay@datalogics.com>
2023-08-22 11:06:34 -0500

    Back out b2 portable recipe changes from upstream conan-io
    - Sparc is failing to build several b2 recipes during CI. It seems
      that the changes in the https://github.com/conan-io/conan-center-index/pull/17882/files
      could be a possible cause for this. Let's try backing out the changes
      to the b2 portable recipe to see if b2 will build for Sparc.

--- recipes/b2/portable/conanfile.py
+++ recipes/b2/portable/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import cross_building
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import chdir, copy, get
 from conan.tools.layout import basic_layout
 
@@ -108,12 +107,6 @@ class B2Conan(ConanFile):
             os.environ.clear()
             os.environ.update(saved_env)
 
-    def _write_project_config(self, cxx):
-        with open(os.path.join(self.source_folder, "project-config.jam"), "w") as f:
-            f.write(
-                f"using {self.options.toolset} : : {cxx} ;\n"
-            )
-
     def build(self):
         # The order of the with:with: below is important. The first one changes
         # the current dir. While the second does env changes that guarantees
@@ -146,14 +139,11 @@ class B2Conan(ConanFile):
         command += "build" if use_windows_commands else "./build.sh"
 
         if self.options.use_cxx_env:
-            envvars = VirtualBuildEnv(self).vars()
-
-            cxx = envvars.get("CXX")
+            cxx = os.environ.get("CXX")
             if cxx:
                 command += f" --cxx={cxx}"
-                self._write_project_config(cxx)
+            cxxflags = os.environ.get("CXXFLAGS")
 
-            cxxflags = envvars.get("CXXFLAGS")
             if cxxflags:
                 command += f" --cxxflags={cxxflags}"
 
